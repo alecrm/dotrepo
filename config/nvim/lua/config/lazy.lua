559b3ca1bae8a63db544bccfc63a8971cb086f59
Alec Millar <alec.millar@str.us>
2025-04-09 11:40:11 -0400

    Added monokai-pro theme
    - Added colorscheme.lua file for handling theme
    - Updated lazy.lua for theme config setup

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -29,7 +29,17 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "monokai-pro", "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  ui = {
+    icons = {
+      ft = "",
+      lazy = "󰂠 ",
+      loaded = " ",
+      not_loaded = " ",
+    },
+  },
 })
+
+vim.cmd.colorscheme("monokai-pro")
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,38 @@
 return {
   {
-    "tanvirtin/monokai.nvim",
-    priority = 1000, -- load it early
-    config = function()
-      vim.cmd("colorscheme monokai")
-    end
-  }
+    "loctvl842/monokai-pro.nvim",
+    lazy = false,
+    priority = 1000,
+    keys = { { "<leader>C", "<cmd>MonokaiProSelect<cr>", desc = "Select Monokai pro filter" } },
+    opts = {
+      transparent_background = false,
+      devicons = true,
+      filter = "pro",
+      day_night = {
+        enable = true,
+        day_filter = "pro",
+        night_filter = "spectrum"
+      },
+      inc_search = "background",
+      background_clear = {
+        "nvim-tree",
+        "bufferline",
+        "telescope",
+        "toggleterm"
+      },
+      plugins = {
+        bufferline = {
+          underline_selected = true,
+          underline_visible = false,
+          underline_fill = true,
+          bold = false,
+        },
+        indent_blankline = {
+          context_highlight = "pro",
+          context_start_underline = true,
+        }
+      }
+    },
+  },
 }
 
