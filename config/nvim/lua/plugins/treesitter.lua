234137391a800e31867d20a8cb76c566e3f9d137
Alec Millar <alec.millar@str.us>
2025-04-09 11:45:55 -0400

    Updated some plugins per monokai-dev's nvim

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,22 +2,81 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    version = false,
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash", "lua", "vim", "vimdoc", "markdown", "markdown_inline",
-          "python", "javascript", "typescript", "html", "css", "json", "yaml"
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true
-        },
-      })
+    event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require("lazy.core.loader").add_to_rtp(plugin)
+      require("nvim-treesitter.query_predicates")
+    end,
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    opts_extend = { "ensure_installed" },
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true, disable = { "yaml", "html" } },
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+      -- incremental_selection = {
+      --   enable = true,
+      --   keymaps = {
+      --     init_selection = "<C-space>",
+      --     node_incremental = "<C-space>",
+      --     scope_incremental = false,
+      --     node_decremental = "<bs>",
+      --   },
+      -- },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
     end
-  }
+  },
+
+  -- Show context of the current function
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    enabled = true,
+    opts = { mode = "cursor", max_lines = 1, zindex = 20 },
+    keys = {
+      { "<leader>tt", "<cmd>TSContextToggle<cr>", desc = "Toggle Treesitter Context" },
+    },
+  },
+
+  -- Automatically add closing tags for HTML and JSX
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {},
+  },
 }
 
--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -6,14 +6,93 @@ return {
       require("lualine").setup()
     end,
   },
+
   {
     "nvim-tree/nvim-web-devicons",
     lazy = true,
   },
+
   {
     "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {},
+    event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+    opts = function()
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.SCOPE_ACTIVE, function(bufnr)
+        return vim.api.nvim_buf_line_count(bufnr) < 2000
+      end)
+
+    local highlight = {
+        "RainbowDelimiterRed",
+        "RainbowDelimiterYellow",
+        "RainbowDelimiterBlue",
+        "RainbowDelimiterOrange",
+        "RainbowDelimiterGreen",
+        "RainbowDelimiterViolet",
+        "RainbowDelimiterCyan",
+      }
+      return {
+        debounce = 200,
+        indent = {
+          char = "▏",
+          tab_char = "▏",
+          -- highlight = "IndentBlanklineChar",
+          -- highlight = highlight,
+        },
+        scope = {
+          injected_languages = true,
+          highlight = highlight,
+          enabled = true,
+          show_start = true,
+          show_end = false,
+          char = "▏",
+          -- include = {
+          --   node_type = { ["*"] = { "*" } },
+          -- },
+          -- exclude = {
+          --   node_type = { ["*"] = { "source_file", "program" }, python = { "module" }, lua = { "chunk" } },
+          -- },
+        },
+        exclude = {
+          filetypes = {
+            "help",
+            "startify",
+            "dashboard",
+            "packer",
+            "neogitstatus",
+            "NvimTree",
+            "Trouble",
+            "alpha",
+            "neo-tree",
+          },
+          buftypes = {
+            "terminal",
+            "nofile",
+          },
+        },
+      }
+    end,
+    main = "ibl"
+  },
+
+  {
+    "echasnovski/mini.hipatterns",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    desc = "Highlight colors in your code. Also includes Tailwind CSS support.",
+    opts = function()
+      local hi = require("mini.hipatterns")
+      return {
+        highlighters = {
+          -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+          fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+          hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+          todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+          note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+          wip = { pattern = "%f[%w]()WIP()%f[%W]", group = "MiniHipatternsWip" },
+          -- Highlight hex color strings (`#rrggbb`) using that color
+          hex_color = hi.gen_highlighter.hex_color({ priority = 2000 }),
+        },
+      }
+    end,
   },
 }
 
