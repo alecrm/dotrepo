0daacc22ca33f89d0aa0f93d9db19641965a4b7e
Alec Millar <alec.millar@str.us>
2025-04-09 22:46:00 -0400

    Added editor.lua for editor-specific plugins

---
+++ config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,188 @@
+return {
+  -- Fold
+  {
+    "kevinhwang91/nvim-ufo",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    branch = "main",
+    version = false,
+    dependencies = { "kevinhwang91/promise-async", event = "BufReadPost" },
+    init = function()
+      vim.o.foldenable = true
+      vim.o.foldcolumn = 'auto:9'
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
+    end,
+    opts = {
+      fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = ("  … %d "):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, "MoreMsg" })
+        return newVirtText
+      end,
+      open_fold_hl_timeout = 0,
+    },
+    keys = {
+      { "fd", "zd", desc = "Delete fol[48;83;318;1411;2544td under cursor" },
+      { "fo", "zo", desc = "Open fold under cursor" },
+      { "fO", "zO", desc = "Open all folds under cursor" },
+      { "fc", "zc", desc = "Close fold under cursor" },
+      { "fC", "zC", desc = "Close all folds under cursor" },
+      { "fa", "za", desc = "Toggle fold under cursor" },
+      { "fA", "zA", desc = "Toggle all folds under cursor" },
+      { "fv", "zv", desc = "Show cursor line" },
+      {
+        "fM",
+        function()
+          require("ufo").closeAllFolds()
+        end,
+        desc = "Close all folds",
+      },
+      {
+        "fR",
+        function()
+          require("ufo").openAllFolds()
+        end,
+        desc = "Open all folds",
+      },
+      {
+        "fm",
+        function()
+          require("ufo").closeFoldsWith()
+        end,
+        desc = "Fold more",
+      },
+      {
+        "fr",
+        function()
+          require("ufo").openFoldsExceptKinds()
+        end,
+        desc = "Fold less",
+      },
+      { "fx", "zx", desc = "Update folds" },
+      { "fz", "zz", desc = "Center this line" },
+      { "ft", "zt", desc = "Top this line" },
+      { "fb", "zb", desc = "Bottom this line" },
+      { "fg", "zg", desc = "Add word to spell list" },
+      { "fw", "zw", desc = "Mark word as bad/misspelling" },
+      { "fe", "ze", desc = "Right this line" },
+      { "fE", "zE", desc = "Delete all folds in current buffer" },
+      { "fs", "zs", desc = "Left this line" },
+      { "fH", "zH", desc = "Half screen to the left" },
+      { "fL", "zL", desc = "Half screen to the right" },
+    },
+  },
+
+  -- Trouble code diagnostics
+  {
+    "folke/trouble.nvim",
+    cmd = { "Trouble" },
+    opts = {
+      modes = {
+        lsp = {
+          win = { position = "right" },
+        },
+      },
+    },
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols (Trouble)" },
+      { "<leader>cS", "<cmd>Trouble lsp toggle<cr>", desc = "LSP references/definitions/... (Trouble)" },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+      {
+        "[q",
+        function()
+          if require("trouble").is_open() then
+            require("trouble").prev({ skip_groups = true, jump = true })
+          else
+            local ok, err = pcall(vim.cmd.cprev)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
+          end
+        end,
+        desc = "Previous Trouble/Quickfix Item",
+      },
+      {
+        "]q",
+        function()
+          if require("trouble").is_open() then
+            require("trouble").next({ skip_groups = true, jump = true })
+          else
+            local ok, err = pcall(vim.cmd.cnext)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
+          end
+        end,
+        desc = "Next Trouble/Quickfix Item",
+      },
+    },
+  },
+
+  -- Status column for who knows what
+  {
+    "luukvbaal/statuscol.nvim",
+    event = { "VimEnter" }, -- Enter when on Vim startup to setup folding correctly (Avoid number in fold column)
+    commit = (function()
+      if vim.fn.has("nvim-0.9") == 1 then
+        return "483b9a596dfd63d541db1aa51ee6ee9a1441c4cc"
+      end
+    end)(),
+    config = function()
+      local builtin = require("statuscol.builtin")
+      require("statuscol").setup({
+        relculright = false,
+        ft_ignore = { "nvim-tree" },
+        segments = {
+          {
+            -- line number
+            text = { " ", builtin.lnumfunc },
+            condition = { true, builtin.not_empty },
+            click = "v:lua.ScLa",
+          },
+          { text = { "%s" }, click = "v:lua.ScSa" }, -- Sign
+          { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" }, -- Fold
+        },
+      })
+      vim.api.nvim_create_autocmd({ "BufEnter" }, {
+        callback = function()
+          if vim.bo.filetype == "nvim-tree" then
+            vim.opt_local.statuscolumn = ""
+          end
+        end,
+      })
+    end,
+  },
+
+  {
+    "moll/vim-bbye",
+    event = { "BufRead" },
+    keys = {
+      { "<leader>d", "<cmd>Bdelete!<cr>", desc = "Close Buffer" },
+    },
+  },
+}
