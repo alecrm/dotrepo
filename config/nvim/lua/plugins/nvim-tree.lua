64f14ea109b90956f8c652ac08570cad9e220d00
Alec Millar <alec.millar@str.us>
2025-04-17 11:39:42 -0400

    Cleaned up nvim-tree. Made it respect project root

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,11 +1,13 @@
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    {"<leader>e", ":NvimTreeToggle<CR>", desc = "Open filetree"}
-  },
   config = function()
 		require("nvim-tree").setup({
+      root_dirs            = { ".git", "package.json", "Makefile" },  -- define your project markers
+      update_focused_file = {
+        enable     = true,   -- keep the tree in sync with the buffer
+        update_root = true,  -- if the buffer is outside the current root, jump the tree to its root dir
+      },
       git = {
         ignore = false,
       },
@@ -22,7 +24,7 @@ return {
         },
       },
 			vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-	})
+    })
   end
 }
 
